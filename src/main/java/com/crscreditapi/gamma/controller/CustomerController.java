46dc7f0390787e294caf28d7b0eced8243fc8982
Andrew Dudley <andrew.sutton.dudley@gmail.com>
2023-06-19 10:43:28 -0400

    Controller test passes

--- src/main/java/com/crscreditapi/gamma/controller/CustomerController.java
+++ src/main/java/com/crscreditapi/gamma/controller/CustomerController.java
@@ -1,19 +1,25 @@
 package com.crscreditapi.gamma.controller;
 
 import com.crscreditapi.gamma.model.Customer;
+import com.crscreditapi.gamma.service.CustomerService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
     public List<Customer> findAll() {
-        return new ArrayList<>();
+        return customerService.findAll();
     }
 }
---
+++ src/main/java/com/crscreditapi/gamma/service/CustomerService.java
@@ -0,0 +1,21 @@
+package com.crscreditapi.gamma.service;
+
+import com.crscreditapi.gamma.model.Customer;
+import com.crscreditapi.gamma.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+}
